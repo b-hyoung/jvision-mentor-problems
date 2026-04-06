6879f5a74dcd16bfa64622a6f9c72c134c4c71b4
kimhyeonwoo484 <kimzna1222@gmail.com>
2026-04-06 19:46:32 +0900

    kimhyeonwoo484.c

--- answer/problem001/kimhyeonwoo484.c
+++ answer/problem001/kimhyeonwoo484.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+int main() {
+    int N;
+    printf("1부터 100까지 중의 정수를 입력하세요 : \n");
+    scanf("%d",&N);
+    for(int i = 1; i<=N; i++){
+        for (int j = 0; j < i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
     return 0;
 }
