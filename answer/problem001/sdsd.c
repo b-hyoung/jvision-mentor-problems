a74fd6232c72930094e0ef78c6ae9e0d9945dd3d
dbgusrl2247 <yhk041018@gmail.com>
2026-04-06 18:14:11 +0900

    123
    123

--- answer/problem001/sdsd.c
+++ answer/problem001/sdsd.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    scanf("%d", &n);
-
-    for(int a=1; a<=n; a++){
-        for(int b=1; b<=a; b++){
+    int N;
+    scanf("%d", &N);
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j < i; j++) {
             printf("*");
-}
+        }
         printf("\n");
-}      
+    }
     return 0;
 }
