76eb69f504ab7087d5c5af43506f2fa78e878f5c
zntks1234 <zntks1234@gmail.com>
2026-04-08 21:58:11 +0900

    Chu002 (#22)
    * cho002
    
    * Update chu002.cpp

---
+++ answer/problem002/chu002.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    
+    int i=0,j=0,n;
+    scanf("%d",&n);
+    int start =n, end =n+1;
+    for(i=0;i<n;i++){
+        for(j=1;j<start;j++){
+            printf(" ");
+        }
+        for(j=start;j<end;j++){
+            printf("*");
+        }
+ 		start--;
+ 		end++;
+        printf("\n");
+    }
+    return 0;
+}
