3f36adebb031ec3cb3138dcb7ce7ea7cc3cac62b
cvvv1225-ui <cvvv1225@gmail.com>
2026-04-17 14:03:29 +0900

    Add files via upload

---
+++ ppppp.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+int n;
+scanf("%d", &n);
+for(int a=1; a<=n; a++){
+    for(int b=1; b<=n-a; b++){
+        printf(" ");
+    }
+    for(int b=1; b<=a; b++){
+        printf("*");
+    }
+    for(int b=1; b<=a-1; b++){
+        printf("*");
+    }
+    printf("\n");
+}
+    
+    return 0;
+}
