eec737159d6ad6a673164060f09724f113adef6b
dododo00096-rgb <dododo00096@gmail.com>
2026-05-06 22:00:41 +0900

    Update inu_003.cpp

--- answer/problem003/inu_003.cpp
+++ answer/problem003/inu_003.cpp
@@ -1,36 +1,35 @@
 #include <stdio.h>
-int main(){
 
-int arr[100][100]={0};
-int num=0;
-int result=0;
-printf("숫자 입력:\n");
-scanf("%d",&num);
-for(int i=0; i<num; i++){
-for(int j=0; j<num; j++){
-scanf("%d",&arr[i][j]);
-}
-}
-for(int i=0; i<num; i++){
-for(int j=0; j<num; j++){
-printf("%2d ",arr[i][j]);
-}
-printf("\n\n");
-}
-for(int u=1; u<num-1; u++){
-result+=arr[1][u];
-result+=arr[u][1];
+int main() {
 
-}
-for(int n=2; n<num-2; n++){
-result+=arr[n][1];
-result+=arr[n][num-2];
-}
-printf("두번째 껍데기 값:");
-printf("%d",result);
-//좌측 상단 1,1
-//좌측 하단 1,num-2
-//우측 상단 num-2,1
-//우측 하단 num-2,num-2
-return 0;
+    int num=0;
+    int result=0;
+    int arr[100][100]={0};
+    scanf("%d",&num);
+
+
+    for(int i=0; i<num; i++){
+        for(int j=0; j<num; j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i=0; i<num; i++){
+        for(int j=0; j<num; j++){
+            printf("%2d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(int i=1; i<=num-2; i++){
+        result += arr[num-2][i];
+        result += arr[1][i];
+    }
+
+    for(int i=2; i<=num-3; i++){
+        result += arr[i][1];
+        result += arr[i][num-2];
+    }
+    
+    printf("%d",result);
+    return 0;
 }
